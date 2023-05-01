488712585a92a14ef49cb2134b01c17e5ccb0c3b
Vinicius Recoaro <vrecoaro@hotmail.com>
2023-05-01 12:03:29 -0300

    Add month expense field on main screen and add option to turn off the total expense visible

--- app/src/main/java/com/example/fico/ui/MainActivity.kt
+++ app/src/main/java/com/example/fico/ui/MainActivity.kt
@@ -3,6 +3,9 @@ package com.example.fico.ui
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.InputType
+import android.text.method.PasswordTransformationMethod
+import com.example.fico.R
 import com.example.fico.databinding.ActivityAddExpenseBinding
 import com.example.fico.databinding.ActivityMainBinding
 
@@ -13,12 +16,29 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
+    }
+
+    override fun onResume() {
+        super.onResume()
         setUpListeners()
     }
 
     private fun setUpListeners(){
         binding.btAddExpenses.setOnClickListener {
             startActivity(Intent(this, AddExpenseActivity::class.java))
+        }
+        binding.tvTotalExpensesValue.setOnClickListener {
+            if (binding.tvTotalExpensesValue.inputType == InputType.TYPE_TEXT_VARIATION_PASSWORD) {
+                binding.tvTotalExpensesValue.inputType = InputType.TYPE_TEXT_VARIATION_VISIBLE_PASSWORD
+                binding.tvTotalExpensesValue.transformationMethod = null
+                binding.tvTotalExpensesValue.setCompoundDrawablesWithIntrinsicBounds(0, 0, R.drawable.ic_visibility_24, 0)
+            }
+            //if(binding.tvTotalExpensesValue.inputType == InputType.TYPE_TEXT_VARIATION_VISIBLE_PASSWORD){
+            else{
+                binding.tvTotalExpensesValue.inputType = InputType.TYPE_TEXT_VARIATION_PASSWORD
+                binding.tvTotalExpensesValue.transformationMethod = PasswordTransformationMethod()
+                binding.tvTotalExpensesValue.setCompoundDrawablesWithIntrinsicBounds(0, 0, R.drawable.ic_visibility_off_24, 0)
+            }
         }
     }
 
