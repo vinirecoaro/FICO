e1d643f17ecd0e2043e079472fe0281a68ede604
Vinicius Recoaro <vrecoaro@hotmail.com>
2023-05-06 16:17:59 -0300

    Changed the launcher Activity to ActivityLogin and make the connection between Login and Register Activity

--- app/src/main/java/com/example/fico/ui/LoginActivity.kt
+++ app/src/main/java/com/example/fico/ui/LoginActivity.kt
@@ -1,12 +1,28 @@
 package com.example.fico.ui
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.fico.R
+import com.example.fico.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
+
+    private val binding by lazy { ActivityLoginBinding.inflate(layoutInflater) }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        setContentView(binding.root)
+        setUpListeners()
     }
+
+    private fun setUpListeners(){
+        binding.btLogin.setOnClickListener {
+
+        }
+        binding.tvRegister.setOnClickListener{
+            startActivity(Intent(this, RegisterActivity::class.java))
+        }
+    }
+
 }
