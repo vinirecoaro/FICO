b3739843fddc04f204a1d3d9a698aa9cbdd5f377
Vinicius Recoaro <vrecoaro@hotmail.com>
2023-05-18 22:50:03 -0300

    Set the date of today as dafault on AddExpenseActivity and exhibited a datepicker when user click on date editText

--- app/src/main/java/com/example/fico/ui/AddExpenseActivity.kt
+++ app/src/main/java/com/example/fico/ui/AddExpenseActivity.kt
@@ -1,12 +1,15 @@
 package com.example.fico.ui
 
 import android.R
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.InputType
 import android.view.View
 import android.widget.AdapterView
 import android.widget.ArrayAdapter
 import androidx.activity.viewModels
+import androidx.annotation.RequiresApi
 import com.example.fico.databinding.ActivityAddExpenseBinding
 import com.example.fico.model.Expense
 import com.example.fico.service.FirebaseAPI
@@ -19,13 +22,18 @@ class AddExpenseActivity : AppCompatActivity() {
     private val viewModel by viewModels<AddExpenseViewModel>()
 
 
+    @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
         setUpListeners()
         categoryListConfig()
+
+        binding.etDate.setText(viewModel.getCurrentlyDate())
+        binding.etDate.inputType = InputType.TYPE_NULL
     }
 
+    @RequiresApi(Build.VERSION_CODES.O)
     private fun setUpListeners() {
         binding.btSave.setOnClickListener {
             finish()
@@ -57,6 +65,18 @@ class AddExpenseActivity : AppCompatActivity() {
                 binding.etDate.text.toString()
             )
             finish()
+        }
+        binding.etDate.setOnClickListener {
+            binding.dpDateExpense.visibility = View.VISIBLE
+            binding.dpDateExpense.setOnDateChangedListener{_, selectedYear,selectedMonth, selectedDay ->
+                val selectedDate = String.format("%02d/%02d/%04d", selectedDay, selectedMonth + 1, selectedYear)
+                binding.etDate.setText(selectedDate)
+            }
+        }
+        binding.etDate.onFocusChangeListener = View.OnFocusChangeListener { _, hasFocus ->
+            if(!hasFocus){
+                binding.dpDateExpense.visibility = View.GONE
+            }
         }
     }
 
--- app/src/main/java/com/example/fico/ui/viewmodel/AddExpenseViewModel.kt
+++ app/src/main/java/com/example/fico/ui/viewmodel/AddExpenseViewModel.kt
@@ -1,8 +1,12 @@
 package com.example.fico.ui.viewmodel
 
+import android.os.Build
+import androidx.annotation.RequiresApi
 import androidx.lifecycle.ViewModel
 import com.example.fico.model.Expense
 import com.example.fico.service.FirebaseAPI
+import java.time.LocalDate
+import java.time.format.DateTimeFormatter
 
 class AddExpenseViewModel : ViewModel() {
 
@@ -11,5 +15,12 @@ class AddExpenseViewModel : ViewModel() {
     fun addExpense(price: Float, description: String, category: String, date: String){
         val expense = Expense(price, description, category, date)
         firebaseAPI.addExpense(expense)
+    }
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    fun getCurrentlyDate() : String{
+        val currentDate = LocalDate.now()
+        val formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy")
+        return currentDate.format(formatter)
     }
 }
