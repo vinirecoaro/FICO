65df8c12720d692f5c61623cb4e250c9b44f731c
Vinicius Recoaro <vrecoaro@hotmail.com>
2023-08-19 22:59:25 -0300

    Deleted unused activities and changed the context of fragments layouts

--- app/src/main/java/com/example/fico/ui/fragments/ConfigurationFragment.kt
+++ app/src/main/java/com/example/fico/ui/fragments/ConfigurationFragment.kt
@@ -6,7 +6,6 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
-import com.example.fico.databinding.ActivityConfigurationBinding
 import com.example.fico.databinding.FragmentConfigurationBinding
 import com.example.fico.ui.viewmodel.ConfigurationViewModel
 import com.google.android.material.snackbar.Snackbar
