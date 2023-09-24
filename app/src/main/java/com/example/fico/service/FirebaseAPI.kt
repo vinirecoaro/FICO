1a4d65b79c570723934f8bf9adf0f7835a62c9e2
Vinicius Recoaro <vrecoaro@hotmail.com>
2023-09-23 22:49:40 -0300

    Created an function to format the month on filter of expense list fragment

--- app/src/main/java/com/example/fico/service/FirebaseAPI.kt
+++ app/src/main/java/com/example/fico/service/FirebaseAPI.kt
@@ -371,7 +371,8 @@ class FirebaseAPI private constructor() {
             val expenseMonths = mutableListOf<String>()
             override fun onDataChange(snapshot: DataSnapshot) {
                 for(month in snapshot.children){
-                    expenseMonths.add(month.key.toString())
+                    val formattedDate = formatDateForFilterOnExpenseList(month.key.toString())
+                    expenseMonths.add(formattedDate)
                 }
                 continuation.resume(expenseMonths)
             }
@@ -394,6 +395,35 @@ class FirebaseAPI private constructor() {
         }
 
         return sequenciaAleatoria.toString()
+    }
+
+    private fun formatDateForFilterOnExpenseList(date: String) : String{
+        var formattedDate = ""
+        val month = date.substring(5,7)
+        if(month == "01"){
+            formattedDate = "Janeiro - ${date.substring(0,4)}"
+        } else if(month == "02"){
+            formattedDate = "Fevereiro - ${date.substring(0,4)}"
+        } else if(month == "03"){
+            formattedDate = "Março - ${date.substring(0,4)}"
+        } else if(month == "04"){
+            formattedDate = "Abril - ${date.substring(0,4)}"
+        } else if(month == "05"){
+            formattedDate = "Maio - ${date.substring(0,4)}"
+        } else if(month == "07"){
+            formattedDate = "Julho - ${date.substring(0,4)}"
+        } else if(month == "08"){
+            formattedDate = "Agosto - ${date.substring(0,4)}"
+        } else if(month == "09"){
+            formattedDate = "Setembro - ${date.substring(0,4)}"
+        } else if(month == "10"){
+            formattedDate = "Outubro - ${date.substring(0,4)}"
+        } else if(month == "11"){
+            formattedDate = "Novembro - ${date.substring(0,4)}"
+        } else if(month == "12"){
+            formattedDate = "Dezembro - ${date.substring(0,4)}"
+        }
+        return formattedDate
     }
 
 }
