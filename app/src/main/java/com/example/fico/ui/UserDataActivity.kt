e03e57bd8ed47691cd9ece70fe4ef91777a0f512
Vinicius Recoaro <vrecoaro@hotmail.com>
2023-11-15 20:15:20 -0300

    UserDataActivity - Getting email and name from database

--- app/src/main/java/com/example/fico/ui/UserDataActivity.kt
+++ app/src/main/java/com/example/fico/ui/UserDataActivity.kt
@@ -4,12 +4,18 @@ import android.content.res.Configuration
 import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.viewModels
+import androidx.lifecycle.lifecycleScope
 import com.example.fico.R
 import com.example.fico.databinding.ActivityUserDataBinding
+import com.example.fico.ui.viewmodel.UserDataViewModel
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 class UserDataActivity : AppCompatActivity() {
 
     private val binding by lazy {ActivityUserDataBinding.inflate(layoutInflater)}
+    private val viewModel by viewModels<UserDataViewModel>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -17,6 +23,8 @@ class UserDataActivity : AppCompatActivity() {
         binding.toolbar.setTitle("Dados Pessoais")
         binding.toolbar.setTitleTextColor(Color.WHITE)
         setColorBasedOnTheme()
+        getUserEmail()
+        getUserName()
     }
 
     private fun setColorBasedOnTheme(){
@@ -30,6 +38,18 @@ class UserDataActivity : AppCompatActivity() {
                 binding.ivEditName.setImageResource(R.drawable.baseline_edit_black)
             }
             Configuration.UI_MODE_NIGHT_UNDEFINED -> {}
+        }
+    }
+
+    private fun getUserName(){
+        lifecycleScope.launch(Dispatchers.Main){
+            binding.tvNameValue.text = viewModel.getUserName().await()
+        }
+    }
+
+    private fun getUserEmail(){
+        lifecycleScope.launch(Dispatchers.Main){
+            binding.tvEmailValue.text = viewModel.getUserEmail().await()
         }
     }
 }
---
+++ app/src/main/java/com/example/fico/ui/viewmodel/UserDataViewModel.kt
@@ -0,0 +1,26 @@
+package com.example.fico.ui.viewmodel
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.fico.service.FirebaseAPI
+import kotlinx.coroutines.Deferred
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.async
+
+class UserDataViewModel : ViewModel() {
+
+    private val firebaseAPI = FirebaseAPI.instance
+
+    fun getUserEmail() : Deferred<String> =
+        viewModelScope.async{
+            val email = firebaseAPI.getUserEmail()
+            return@async email
+        }
+
+    fun getUserName() : Deferred<String> =
+        viewModelScope.async {
+            val name = firebaseAPI.getUserName()
+            return@async name
+        }
+
+}
