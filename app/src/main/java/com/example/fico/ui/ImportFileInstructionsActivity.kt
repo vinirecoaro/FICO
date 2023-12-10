a2852be9aad267b375bf4e32c332898db7e67997
Vinicius Recoaro <vrecoaro@hotmail.com>
2023-12-10 13:42:20 -0300

    ImportFileInstructionsActivity - Added an viewPager to show the instructions in slide form

--- app/src/main/java/com/example/fico/ui/ImportFileInstructionsActivity.kt
+++ app/src/main/java/com/example/fico/ui/ImportFileInstructionsActivity.kt
@@ -2,30 +2,31 @@ package com.example.fico.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.viewpager2.widget.ViewPager2
 import com.example.fico.R
 import com.example.fico.databinding.ActivityImportFileInstructionsBinding
 import com.example.fico.model.ImportFileInstructionsComponents
+import com.example.fico.ui.adapters.ImportFileInstructionsAdapter
 import com.example.fico.ui.fragments.ImportFileInstructionsFragment
 
 class ImportFileInstructionsActivity : AppCompatActivity() {
 
     private val binding by lazy{ActivityImportFileInstructionsBinding.inflate(layoutInflater)}
+    private lateinit var importFileInstructionsAdapter: ImportFileInstructionsAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
 
-        val contentIndex = 0
-        val fragment = ImportFileInstructionsFragment.newInstance(getImportFileInstructionsComponents()[contentIndex])
+        var contentIndex = 0
 
-        supportFragmentManager.beginTransaction()
-            .replace(binding.flContainer.id, fragment)
-            .commit()
+        importFileInstructionsAdapter = ImportFileInstructionsAdapter(this, getImportFileInstructionsComponents())
+        binding.vpInstructions.adapter = importFileInstructionsAdapter
 
     }
 
     private fun getImportFileInstructionsComponents() : List<ImportFileInstructionsComponents>{
-        val content = listOf(
+        val contents = listOf(
             ImportFileInstructionsComponents(
                 "Extensão do arquivo",
                 R.drawable.ic_add_24,
@@ -42,21 +43,29 @@ class ImportFileInstructionsActivity : AppCompatActivity() {
             ImportFileInstructionsComponents(
                 "Coluna Preço",
                 R.drawable.ic_add_24,
-                "Na coluna preço os valores podem estar no seguinte formato:\n" +
-                        "R$ 20,00\n" +
-                        "$ 20,00\n" +
-                        "20,00",
+                "Na coluna preço os valores podem \nestar no seguinte formato:\n\n" +
+                        "- R$ 20,00\n" +
+                        "- $ 20,00\n" +
+                        "- 20,00",
                 false
             ),
             ImportFileInstructionsComponents(
                 "Coluna Data",
                 R.drawable.ic_add_24,
-                "Na coluna data os valores devem ser no formato:\n" +
+                "Na coluna data os valores devem ser no formato:\n\n" +
                         "dd/mm/aaaa",
                 true
             ),
         )
-        return content
+        return contents
     }
+
+ /*   private fun replaceFragment(contents : List<ImportFileInstructionsComponents>, contentIndex : Int){
+        val fragment = ImportFileInstructionsFragment.newInstance(contents[contentIndex])
+
+        supportFragmentManager.beginTransaction()
+            .replace(binding.flContainer.id, fragment)
+            .commit()
+    }*/
 
 }
---
+++ app/src/main/java/com/example/fico/ui/adapters/ImportFileInstructionsAdapter.kt
@@ -0,0 +1,17 @@
+package com.example.fico.ui.adapters
+
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentActivity
+import androidx.viewpager2.adapter.FragmentStateAdapter
+import com.example.fico.model.ImportFileInstructionsComponents
+import com.example.fico.ui.fragments.ImportFileInstructionsFragment
+
+class ImportFileInstructionsAdapter(fa : FragmentActivity, private val contents : List<ImportFileInstructionsComponents>) : FragmentStateAdapter(fa) {
+    override fun getItemCount(): Int {
+        return contents.size
+    }
+
+    override fun createFragment(position: Int): Fragment {
+        return ImportFileInstructionsFragment.newInstance(contents[position])
+    }
+}
