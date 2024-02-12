ec6aae35eae426a36c867b4d9cc7f5e1476ca586
Vinicius Recoaro <vrecoaro@hotmail.com>
2024-02-12 19:31:39 -0300

    HomeFragment - Updating expense per month chart layout

--- app/src/main/java/com/example/fico/ui/fragments/expense/expense_home/HomeFragment.kt
+++ app/src/main/java/com/example/fico/ui/fragments/expense/expense_home/HomeFragment.kt
@@ -19,6 +19,7 @@ import com.example.fico.databinding.FragmentHomeBinding
 import com.example.fico.ui.viewmodel.HomeViewModel
 import com.github.mikephil.charting.animation.Easing
 import com.github.mikephil.charting.charts.BarChart
+import com.github.mikephil.charting.components.XAxis
 import com.github.mikephil.charting.data.BarData
 import com.github.mikephil.charting.data.BarDataSet
 import com.github.mikephil.charting.data.BarEntry
@@ -34,7 +35,7 @@ class HomeFragment : Fragment(){
     private var _binding : FragmentHomeBinding? = null
     private val binding get() = _binding!!
     private val viewModel by viewModels<HomeViewModel>()
-    lateinit var barChartEntries : ArrayList<BarEntry>
+    private var barChartEntries : ArrayList<BarEntry> = arrayListOf()
 
     @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
@@ -49,7 +50,7 @@ class HomeFragment : Fragment(){
             R.drawable.ic_visibility_off_24,
             0)
         setUpListeners()
-        initExpenseEachMonthChart()
+        initExpenseEachMonthChartEmpty()
 
         return rootView
     }
@@ -61,9 +62,11 @@ class HomeFragment : Fragment(){
         viewModel.infoPerMonthLiveData.observe(viewLifecycleOwner){ infoPerMonthList ->
             var i = 1f
             for (infoPerMonth in infoPerMonthList){
-                barChartEntries.add(BarEntry(i, infoPerMonth.monthExpense.toFloat()))
+                val monthExpense = infoPerMonth.monthExpense.toFloat()
+                barChartEntries.add(BarEntry(i, monthExpense))
                 i += 1f
             }
+            initExpenseEachMonthChart()
         }
 
     }
@@ -118,7 +121,48 @@ class HomeFragment : Fragment(){
         barChart.data = barData
         barChart.legend.isEnabled = false
         barChart.description.isEnabled = false
-        barChart.xAxis.isEnabled = false
+        val xAxis = barChart.xAxis
+        xAxis.position = XAxis.XAxisPosition.BOTTOM
+        xAxis.setDrawGridLines(false)
+        xAxis.setDrawLabels(true)
+
+        // Atualize o gráfico
+        barChart.invalidate()
+
+    }
+
+    private fun initExpenseEachMonthChartEmpty(){
+        val barChart = binding.bcExpenseEachMonth
+
+        val entries = ArrayList<BarEntry>()
+        entries.add(BarEntry(1f, 20f))
+        entries.add(BarEntry(2f, 15f))
+        entries.add(BarEntry(3f, 30f))
+        entries.add(BarEntry(4f, 25f))
+        entries.add(BarEntry(5f, 35f))
+
+        // Crie um conjunto de dados com a lista de entradas
+        val dataSet = BarDataSet(entries, "Label") // "Label" é o nome da legenda
+        //dataSet.valueTextColor = Color.WHITE
+        dataSet.valueTextSize = 12f
+
+        // Personalize as cores do conjunto de dados
+        dataSet.color = Color.BLUE
+
+        // Crie um objeto BarData e defina o conjunto de dados
+        val barData = BarData(dataSet)
+
+        // Configure o espaçamento entre as barras
+        barData.barWidth = 0.5f
+
+        // Defina os dados para o gráfico de barras
+        barChart.data = barData
+        barChart.legend.isEnabled = false
+        barChart.description.isEnabled = false
+        val xAxis = barChart.xAxis
+        xAxis.position = XAxis.XAxisPosition.BOTTOM
+        xAxis.setDrawGridLines(false)
+        xAxis.setDrawLabels(true)
 
         // Atualize o gráfico
         barChart.invalidate()
