8de8f4cc170b87048311961c96c8a5a07620b56a
Vinicius Recoaro <vrecoaro@hotmail.com>
2024-10-30 12:53:48 -0300

    ExpenseListAdapter - Added some conditions to separe earnings from expenses

--- app/src/main/java/com/example/fico/presentation/adapters/ExpenseListAdapter.kt
+++ app/src/main/java/com/example/fico/presentation/adapters/ExpenseListAdapter.kt
@@ -12,11 +12,12 @@ import com.example.fico.model.Earning
 import com.example.fico.model.Expense
 import com.example.fico.model.TransactionCategory
 import com.example.fico.presentation.interfaces.OnListItemClick
+import org.apache.poi.ss.formula.functions.T
 import java.text.DecimalFormat
 import java.text.NumberFormat
 import java.util.*
 
-class ExpenseListAdapter(private var expenseList: List<Expense>, private val earningList : List<Earning>, private val categories : List<TransactionCategory>) : RecyclerView.Adapter<ExpenseListAdapter.ViewHolder>(){
+class ExpenseListAdapter(private var expenseList: List<Expense>, private var earningList : List<Earning>, private val categories : List<TransactionCategory>) : RecyclerView.Adapter<ExpenseListAdapter.ViewHolder>(){
 
     private var listener: OnListItemClick? = null
 
@@ -33,43 +34,86 @@ class ExpenseListAdapter(private var expenseList: List<Expense>, private val ear
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        val item = expenseList[position]
-        holder.description.text = item.description
 
-        val regex = Regex("[\\d,.]+")
-        val justNumber = regex.find(item.price)
-        val formatNum = DecimalFormat("#.##")
-        val justNumberValue = justNumber!!.value.replace(",",".").toFloat()
-        val formattedNum = formatNum.format(justNumberValue).replace(",",".").toFloat()
-        val currencyFormat = NumberFormat.getCurrencyInstance(Locale.getDefault())
-        val formattedPrice = currencyFormat.format(formattedNum)
-        holder.price.text = formattedPrice
-        holder.price.setTextColor(Color.RED)
+        if(position < expenseList.size){
+            val item = expenseList[position]
 
-        holder.date.text = item.paymentDate
+            holder.description.text = item.description
 
-        val itemCategory = item.category
-        val categoryPathName = categories.find{ it.description == itemCategory }
-        if(categoryPathName != null){
-            val iconName = categoryPathName.iconName
-            val iconPath = holder.itemView.context.resources.getIdentifier(iconName, "drawable", holder.itemView.context.packageName)
-            holder.categoryImg.setImageResource(iconPath)
+            val regex = Regex("[\\d,.]+")
+            val justNumber = regex.find(item.price)
+            val formatNum = DecimalFormat("#.##")
+            val justNumberValue = justNumber!!.value.replace(",",".").toFloat()
+            val formattedNum = formatNum.format(justNumberValue).replace(",",".").toFloat()
+            val currencyFormat = NumberFormat.getCurrencyInstance(Locale.getDefault())
+            val formattedPrice = currencyFormat.format(formattedNum)
+            holder.price.text = formattedPrice
+            holder.price.setTextColor(Color.RED)
+
+            holder.date.text = item.paymentDate
+
+            val itemCategory = item.category
+            val categoryPathName = categories.find{ it.description == itemCategory }
+            if(categoryPathName != null){
+                val iconName = categoryPathName.iconName
+                val iconPath = holder.itemView.context.resources.getIdentifier(iconName, "drawable", holder.itemView.context.packageName)
+                holder.categoryImg.setImageResource(iconPath)
+            }else{
+                val iconPath = holder.itemView.context.resources.getIdentifier("baseline_cancel_dark", "drawable", holder.itemView.context.packageName)
+                holder.categoryImg.setImageResource(iconPath)
+            }
+
+            holder.itemView.setOnClickListener {
+                listener?.onListItemClick(position)
+            }
         }else{
-            val iconPath = holder.itemView.context.resources.getIdentifier("baseline_cancel_dark", "drawable", holder.itemView.context.packageName)
-            holder.categoryImg.setImageResource(iconPath)
+            val item = earningList[position-expenseList.size]
+
+            holder.description.text = item.description
+
+            val regex = Regex("[\\d,.]+")
+            val justNumber = regex.find(item.value)
+            val formatNum = DecimalFormat("#.##")
+            val justNumberValue = justNumber!!.value.replace(",",".").toFloat()
+            val formattedNum = formatNum.format(justNumberValue).replace(",",".").toFloat()
+            val currencyFormat = NumberFormat.getCurrencyInstance(Locale.getDefault())
+            val formattedPrice = currencyFormat.format(formattedNum)
+            holder.price.text = formattedPrice
+            holder.price.setTextColor(Color.RED)
+
+            holder.date.text = item.date
+
+            val itemCategory = item.category
+            val categoryPathName = categories.find{ it.description == itemCategory }
+            if(categoryPathName != null){
+                val iconName = categoryPathName.iconName
+                val iconPath = holder.itemView.context.resources.getIdentifier(iconName, "drawable", holder.itemView.context.packageName)
+                holder.categoryImg.setImageResource(iconPath)
+            }else{
+                val iconPath = holder.itemView.context.resources.getIdentifier("baseline_cancel_dark", "drawable", holder.itemView.context.packageName)
+                holder.categoryImg.setImageResource(iconPath)
+            }
+
+            holder.itemView.setOnClickListener {
+                listener?.onListItemClick(position)
+            }
         }
 
-        holder.itemView.setOnClickListener {
-            listener?.onListItemClick(position)
-        }
+
     }
 
     override fun getItemCount(): Int {
-        return expenseList.size
+        val fullSize = expenseList.size + earningList.size
+        return fullSize
     }
 
     fun updateExpenses(newExpenses: List<Expense>){
         expenseList = newExpenses
+        notifyDataSetChanged()
+    }
+
+    fun updateEarnings(newEarnings: List<Earning>){
+        earningList = newEarnings
         notifyDataSetChanged()
     }
 
