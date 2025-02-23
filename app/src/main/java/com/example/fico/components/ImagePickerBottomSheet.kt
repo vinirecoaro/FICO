554a4f677956de44f4a1e9f19fd0f9bf510817d0
Vinicius Recoaro <vrecoaro@hotmail.com>
2025-02-23 13:17:10 -0300

    UserDataActivity - Finished imagePickerBottomSheet layout

--- app/src/main/java/com/example/fico/components/ImagePickerBottomSheet.kt
+++ app/src/main/java/com/example/fico/components/ImagePickerBottomSheet.kt
@@ -1,31 +1,60 @@
 package com.example.fico.components
 
+import android.content.res.Configuration
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.example.fico.R
 import com.example.fico.databinding.FragmentImagePickerBottomSheetBinding
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 
 class ImagePickerBottomSheet(private val onOptionSelected: (Boolean) -> Unit) :
     BottomSheetDialogFragment() {
 
+    private lateinit var binding: FragmentImagePickerBottomSheetBinding
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val binding = FragmentImagePickerBottomSheetBinding.inflate(inflater, container, false)
+        binding = FragmentImagePickerBottomSheetBinding.inflate(inflater, container, false)
 
-        binding.btnCamera.setOnClickListener {
+        setUpListeners()
+        setColorBasedOnTheme()
+
+        return binding.root
+
+    }
+
+    private fun setUpListeners(){
+        binding.llBottomSheetCamera.setOnClickListener {
             onOptionSelected(true)
             dismiss()
         }
 
-        binding.btnGallery.setOnClickListener {
+        binding.llBottomSheetGallery.setOnClickListener {
             onOptionSelected(false)
             dismiss()
         }
-
-        return binding.root
     }
+
+    private fun setColorBasedOnTheme() {
+        when (context?.resources?.configuration?.uiMode?.and(Configuration.UI_MODE_NIGHT_MASK)) {
+            Configuration.UI_MODE_NIGHT_YES -> {
+                binding.ivBottomSheetCamera.setImageResource(R.drawable.photo_light)
+                binding.ivBottomSheetGallery.setImageResource(R.drawable.gallery_light)
+
+            }
+
+            Configuration.UI_MODE_NIGHT_NO -> {
+                binding.ivBottomSheetCamera.setImageResource(R.drawable.photo_dark)
+                binding.ivBottomSheetGallery.setImageResource(R.drawable.gallery_dark)
+            }
+
+            Configuration.UI_MODE_NIGHT_UNDEFINED -> {}
+        }
+    }
+
+
 }
