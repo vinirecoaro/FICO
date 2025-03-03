17fc96dd8e6de8baabcf3524472181557ecec0cf
Vinicius Recoaro <vrecoaro@hotmail.com>
2025-03-02 21:36:53 -0300

    FirebaseAuthRepository - UserRepository - setted auth methods

--- app/src/main/java/com/example/fico/repositories/FirebaseAuthRepository.kt
+++ app/src/main/java/com/example/fico/repositories/FirebaseAuthRepository.kt
@@ -1,4 +1,38 @@
 package com.example.fico.repositories
 
-class FirebaseAuthRepository {
+import com.example.fico.model.User
+import com.google.android.gms.tasks.Task
+import com.google.firebase.auth.AuthResult
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+
+class FirebaseAuthRepository(private val auth: FirebaseAuth) {
+
+    suspend fun currentUser(): FirebaseUser? = withContext(Dispatchers.IO) {
+        return@withContext auth.currentUser
+    }
+
+    suspend fun createUser(user: User): Task<AuthResult> = withContext(Dispatchers.IO) {
+        return@withContext auth.createUserWithEmailAndPassword(user.email, user.password)
+    }
+
+    suspend fun login(user: User): Task<AuthResult> = withContext(Dispatchers.IO) {
+        return@withContext auth.signInWithEmailAndPassword(user.email, user.password)
+    }
+
+    suspend fun logoff() = withContext(Dispatchers.IO) {
+        return@withContext auth.signOut()
+    }
+
+    suspend fun getUserEmail(): Result<String> = withContext(Dispatchers.IO) {
+        try {
+            val email = currentUser()?.email ?: throw Exception("User not logged in")
+            Result.success(email)
+        } catch (e: Exception) {
+            Result.failure(e)
+        }
+    }
+
 }
--- app/src/main/java/com/example/fico/repositories/UserRepository.kt
+++ app/src/main/java/com/example/fico/repositories/UserRepository.kt
@@ -1,4 +1,21 @@
 package com.example.fico.repositories
 
-class UserRepository() {
+import com.example.fico.model.User
+import com.google.android.gms.tasks.Task
+import com.google.firebase.auth.AuthResult
+
+class UserRepository(private val firebaseAuthRepository: FirebaseAuthRepository) {
+
+    suspend fun getUserEmail(): Result<String> {
+        return firebaseAuthRepository.getUserEmail()
+    }
+
+    suspend fun login(user: User): Task<AuthResult> {
+        return firebaseAuthRepository.login(user)
+    }
+
+    suspend fun logout() {
+        firebaseAuthRepository.logoff()
+    }
+
 }
